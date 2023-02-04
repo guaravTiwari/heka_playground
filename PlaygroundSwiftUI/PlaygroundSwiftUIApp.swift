0654b0165a58bd62244db5eaf8d905c0de759244
Gaurav Tiwari <gaurav@obvious.in>
2023-02-04 22:38:06 +0530

    Initial Commit

---
+++ PlaygroundSwiftUI/PlaygroundSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundSwiftUIApp.swift
+//  PlaygroundSwiftUI
+//
+//  Created by Gaurav Tiwari on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
